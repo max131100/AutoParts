50204ef86581abdd180f3037a3ea485f64eac72e
max131100 <mkirichenko2308@gmail.com>
2023-06-21 20:46:52 +0300

    Added model Category and CRUD for it

---
+++ app/Models/Category.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+* @mixin Builder
+*/
+class Category extends Model
+{
+    protected $table = 'categories';
+    protected $guarded = false;
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, '__invoke'])->name('main.index');
 
+Route::group(['prefix' => 'categories'], function () {
+    Route::get('/', [App\Http\Controllers\Category\IndexController::class, '__invoke'])->name('category.index');
+    Route::get('/create', [App\Http\Controllers\Category\CreateController::class, '__invoke'])->name('category.create');
+    Route::post('/', [App\Http\Controllers\Category\StoreController::class, '__invoke'])->name('category.store');
+    Route::get('/{category}/edit', [App\Http\Controllers\Category\EditController::class, '__invoke'])->name('category.edit');
+    Route::get('/{category}', [App\Http\Controllers\Category\ShowController::class, '__invoke'])->name('category.show');
+    Route::patch('/{category}', [App\Http\Controllers\Category\UpdateController::class, '__invoke'])->name('category.update');
+    Route::delete('/{category}', [App\Http\Controllers\Category\DeleteController::class, '__invoke'])->name('category.delete');
+});
+
